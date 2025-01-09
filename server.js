444ffe3a722d984d5bf426e1df3b807e49588305
Henrique Dias <riquemdias.510@gmail.com>
2025-01-09 14:32:06 -0300

    Atualizando configurações e ajustes do banco de dados2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const db = mysql.createConnection({
-    host: 'mysql_host_provido_pelo_render', // Altere para o nome do host fornecido pelo Render
+    host: 'https://gpa-cadastro.onrender.com/api', // Altere para o nome do host fornecido pelo Render
     user: 'root',
     password: 'Mudar-55',
     database: 'gpa_cadastro',
