d1d89fb673fde5f46778a06b7df0d7fbdb06bdf8
Henrique Dias <riquemdias.510@gmail.com>
2025-01-10 16:10:33 -0300

    Agoraaaaaa vai3

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const API_URL = 'http://localhost:3001/api';
+    const API_URL = 'https://gpa-cadastro.onrender.com/api';
     const userForm = document.getElementById('userForm');
     const devolucaoInput = document.getElementById('devolucaoColetor');
     const devolverButton = document.getElementById('devolver');
